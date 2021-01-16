db377ced7c9aeb45bf6adb8eacf01b257b5465ce
dimagoncharenko <dimadima9696@gmail.com>
2021-01-16 17:24:28 +0200

    Added options for unick letters

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,29 +3,63 @@ import { alphabets, getRandomLetter } from '../../utils/';
 
 import './app.scss';
 
-const ANIMATION_DURATION = 500;
+const ANIMATION_DURATION = 1000;
 
 const App = () => {
     const { russianAlphabet } = alphabets;
     const [letter, setLetter] = useState(russianAlphabet[0]);
+    const [fakeLetter, setFakeLetter] = useState();
+    const [isAnimation, setIsAnimation] = useState(false);
+    const [pastLetters, setPastLetters] = useState([]);
+    const [isUnique, setIsUnique] = useState(false);
 
-    const onLetterChange = () => {
+    const animationChangeLetter = () => {
+        setIsAnimation(true);
         const letterIntervalId = setInterval(() => {
-            let newLetter = getRandomLetter(russianAlphabet);
-            setLetter(newLetter);
+            setFakeLetter(getRandomLetter(russianAlphabet));
         }, 60);
 
-        setTimeout(() => {
-            clearInterval(letterIntervalId);
-        }, ANIMATION_DURATION);
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                clearInterval(letterIntervalId);
+                resolve('letter change');
+            }, ANIMATION_DURATION);
+        });
+    };
+
+    const onAnimationEnd = () => {
+        setIsAnimation(false);
+        setPastLetters([...pastLetters, letter]);
+        if (isUnique) {
+            const filtered = russianAlphabet.filter((l) => ![...pastLetters, letter].includes(l));
+            setLetter(getRandomLetter(filtered));
+        } else {
+            setLetter(getRandomLetter(russianAlphabet));
+        }
+    };
+
+    const onChangeClick = () => {
+        animationChangeLetter().then(onAnimationEnd);
     };
 
     return (
         <div className="app">
-            <div className="letter">{letter}</div>
-            <button className="btn" onClick={onLetterChange}>
+            <div className="letter">{isAnimation ? fakeLetter : letter}</div>
+            <button className="btn" onClick={onChangeClick} disabled={isAnimation} >
                 Сменить букву
             </button>
+            <div className="settings">
+                <h2>Настройки</h2>
+                <label>
+                    <input
+                        type="checkbox"
+                        name="repeat"
+                        onChange={() => setIsUnique((state) => !state)}
+                        checked={isUnique}
+                    />
+                    <span>Буквы должны быть уникальные</span>
+                </label>
+            </div>
         </div>
     );
 };
